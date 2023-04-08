c771e3528f1ba6224bd103dac2a017e38618cc29
Cameron Matherne <cameronpmatherne@gmail.com>
2023-04-07 23:32:31 -0500

    big changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
-import {ModalComponent} from "./modal-component/modal.component";
 
+const NOTE: any = [
+  {
+    id: 1,
+    title: "Test note  ",
+    content: "Note text information goes here"
+  },
+  {
+    id: 2,
+    title: "Test note  ",
+    content: "Note text information goes here"
+  },
+  {
+    id: 3,
+    title: "Test note  ",
+    content: "Note text information goes here"
+  },
+  {
+    id: 4,
+    title: "Test note  ",
+    content: "Note text information goes here"
+  },
+  {
+    id: 5,
+    title: "Test note  ",
+    content: "Note text information goes here"
+  },
+];
 
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-apps';
+  title = 'Angular Notes';
+
+  findNoteById(noteId: string) {
+    return NOTE.find((note: { id: string; }) => note.id === noteId);
+  }
+
+  createNote(title:string, content:string) {
+    const newNote = new NOTE(NOTE.length + 1, title, content);
+    NOTE.push(newNote);
+    console.log(title, content);
+    return newNote;
+  }
 
 }
